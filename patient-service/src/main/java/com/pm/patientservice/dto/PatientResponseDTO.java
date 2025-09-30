f1b9b05b58519dcdb2f1448e4b06c6e60c6251dd
Xuwei <qinxuwei15@gmail.com>
2025-09-30 18:24:38 +0200

    mircoproject init and patient-service module

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,19 @@
+package com.pm.patientservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PatientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private LocalDate dateOfBirth;
+    private LocalDate registeredDate;
+}
